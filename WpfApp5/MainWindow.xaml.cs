41c178f0a9fa6f673d4d64d6cb74369216747325
User <User@DESKTOP-N61M6PA>
2024-08-24 00:56:03 +0400

    removed blank lines

--- WpfApp5/MainWindow.xaml.cs
+++ WpfApp5/MainWindow.xaml.cs
@@ -25,7 +25,6 @@ namespace WpfApp5
                 int num2 = int.Parse(Num2.Text);
                 int den2 = int.Parse(Den2.Text);
 
-
                 if (den1 == den2)
                 {
                     WholeRezult.Text = (whole1 + whole2).ToString();
@@ -35,9 +34,8 @@ namespace WpfApp5
                 else if (den1 != den2)
                 {
                     WholeRezult.Text = (whole1 + whole2).ToString();
-                    DenRezult.Text= (den1 * den2).ToString();
-                    NumRezult.Text =((den2 * num1) + (den1 * num2)).ToString();
-
+                    DenRezult.Text = (den1 * den2).ToString();
+                    NumRezult.Text = ((den2 * num1) + (den1 * num2)).ToString();
                 }
             }
             catch (Exception ex)
@@ -45,16 +43,14 @@ namespace WpfApp5
                 MessageBox.Show("Error: " + ex.Message);
             }
         }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Whole1.Text = null; Whole2.Text = null;
-            Den1.Text = null; Den2.Text = null; 
+            Den1.Text = null; Den2.Text = null;
             Num1.Text = null; Num2.Text = null;
             WholeRezult.Text = null;
             NumRezult.Text = null;
             DenRezult.Text = null;
         }
     }
-
 }
